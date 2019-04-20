91b4499e612f40f41c861e0d2deffcdb1d7b453e
Akansh Mahendra <32019540+akanshmahendra@users.noreply.github.com>
2019-04-21 00:38:56 +0530

    Chapter 2

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'src/app/model/stock/stock.component';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent implements OnInit {
+
+  public stock: Stock;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+
+  }
+
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite state for this stock', event);
+    this.stock.favoite = !this.stock.favoite;
+  }
+
+}
